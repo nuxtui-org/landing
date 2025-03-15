45d4c08b5a18b193949728d46ba4d19182069964
Lbraim <lbraimbranco@gmail.com>
2025-03-15 14:37:58 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui-pro',
+    '@nuxt/content'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  mdc: {
+    highlight: {
+      noApiRoute: false
+    }
+  },
+
+  routeRules: {
+    '/': { prerender: true }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
